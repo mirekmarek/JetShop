311a0753392be4a6241bef8bcbd451052d9db0a3
mirekmarek <mirek.marek.2m@gmail.com>
2022-09-24 18:05:46 +0200

    ...

--- application/_ClassesTrash/Parametrization/Property/Value.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace JetShop;
-
-abstract class Parametrization_Property_Value extends Core_Parametrization_Property_Value {
-
-}
--- application/_ClassesTrash/Stencil/ManageModuleInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace JetShop;
-
-interface Stencil_ManageModuleInterface extends Core_Stencil_ManageModuleInterface{
-
-}
