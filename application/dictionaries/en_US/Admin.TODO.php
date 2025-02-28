f2343550d700a83fec731fc5bea7113941601d67
mirekmarek <mirek.marek.2m@gmail.com>
2025-02-28 13:35:54 +0100

    ...

---
+++ application/dictionaries/en_US/Admin.TODO.php
@@ -0,0 +1,17 @@
+<?php
+return [
+	'TODO' => '',
+	'Visible for' => '',
+	'All' => '',
+	'My private' => '',
+	'Please enter task' => '',
+	'Invalid value' => '',
+	'Please enter a value' => '',
+	'Priority:' => '',
+	'Low' => '',
+	'Normal' => '',
+	'High' => '',
+	'TOP' => '',
+	'Task:' => '',
+	'TODO (%CNT%)' => '',
+];
