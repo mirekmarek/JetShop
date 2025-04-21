1f1d4b3ceec608d6fc6e3b294bbbbb11f6e78339
mirekmarek <mirek.marek.2m@gmail.com>
2025-04-21 23:27:10 +0200

    ...

--- library/JetShop/Core/Admin/Listing/Column.php
+++ library/JetShop/Core/Admin/Listing/Column.php
@@ -9,6 +9,7 @@ namespace JetShop;
 use Jet\Autoloader;
 use Jet\DataListing_Column;
 use Jet\Factory_MVC;
+use Jet\MVC_View;
 
 abstract class Core_Admin_Listing_Column extends DataListing_Column
 {
@@ -19,15 +20,21 @@ abstract class Core_Admin_Listing_Column extends DataListing_Column
 		return static::KEY;
 	}
 	
-	public function render( mixed $item ): string
+	protected function getView() : MVC_View
 	{
-		
 		$view_dir = dirname( Autoloader::getScriptPath( static::class ), 3 ).'/views/list/column/';
 		
 		$view = Factory_MVC::getViewInstance( $view_dir );
-		$view->setVar('item', $item);
 		$view->setVar('listing', $this->listing );
 		$view->setVar('column', $this );
+		
+		return $view;
+	}
+	
+	public function render( mixed $item ): string
+	{
+		$view = $this->getView();
+		$view->setVar('item', $item);
 		
 		return $view->render( $this->getKey() );
 	}
--- library/JetShop/Core/Admin/Listing/Filter.php
+++ library/JetShop/Core/Admin/Listing/Filter.php
@@ -9,6 +9,7 @@ namespace JetShop;
 use Jet\Autoloader;
 use Jet\DataListing_Filter;
 use Jet\Factory_MVC;
+use Jet\MVC_View;
 
 abstract class Core_Admin_Listing_Filter extends DataListing_Filter
 {
@@ -19,17 +20,21 @@ abstract class Core_Admin_Listing_Filter extends DataListing_Filter
 		return static::KEY;
 	}
 	
-	
-	public function renderForm(): string
+	protected function getView() : MVC_View
 	{
-		
 		$view_dir = dirname( Autoloader::getScriptPath( static::class ), 3 ).'/views/list/filter/';
 		
 		$view = Factory_MVC::getViewInstance( $view_dir );
 		$view->setVar('filter', $this );
 		$view->setVar('listing', $this->listing);
 		
-		return $view->render( $this->getKey() );
+		return $view;
+	}
+	
+	
+	public function renderForm(): string
+	{
+		return $this->getView()->render( $this->getKey() );
 	}
 	
 }
