98ebefad91e98fc6d65e1e305b5fedaaf22b60a6
mirekmarek <mirek.marek.2m@gmail.com>
2025-05-16 21:03:57 +0200

    ...

---
+++ application/Modules/Admin/TODO/_install/install.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @copyright Copyright (c) Miroslav Marek <mirek.marek@web-jet.cz>
+ * @license EUPL 1.2  https://eupl.eu/1.2/en/
+ * @author Miroslav Marek <mirek.marek@web-jet.cz>
+ */
+namespace JetApplicationModule\Admin\TODO;
+
+use Jet\DataModel_Helper;
+
+DataModel_Helper::create( Item::class );
